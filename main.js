b966205af2303d0b2e68b27b5fa082c8be16c329
Max Jacob Schridde <nimda@Nimdas-MacBook-Pro.local>
2016-10-23 11:10:53 -0700

    Created first connection b/w companionapp and device - toggle LED simulator from companionapp using Pins.share and Pins.discover

--- main.js
+++ main.js
@@ -1,18 +1,48 @@
-/*
- *     Copyright (C) 2010-2016 Marvell International Ltd.
- *     Copyright (C) 2002-2010 Kinoma, Inc.
- *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
- */
-trace("Hello, World!\n");
-debugger;
+let backgroundSkin = new Skin({ fill : ["#202020", "#7DBF2E"] });
+let textStyle = new Style({ font: "bold 50px", color: "white" });
+
+var Pins = require("pins");
+let remotePins;
+class AppBehavior extends Behavior{
+	onLaunch(application){
+		application.add(new MainContainer());
+		let discoveryInstance = Pins.discover(
+			connectionDesc => {
+				if (connectionDesc.name == 'pins-share-led'){
+					trace('Connection to remote pins\n');
+					remotePins = Pins.connect(connectionDesc);
+				}
+			},
+			connectionDesc => {
+				if (connectionDesc.name == 'pins-share-led'){
+					trace('Disconnected from remote pins\n');
+					remotePins = undefined;
+				}
+			});
+	}
+	onToggleLight(application, value){
+		if (remotePins) remotePins.invoke("/led/write", value);
+	}
+}
+application.behavior = new AppBehavior();
+
+let MainContainer = Container.template($ => ({
+    top: 0, bottom: 0, left: 0, right: 0,
+    active: true, skin: backgroundSkin, state: 0,
+    contents: [
+        Label($, { name: "statusString", top: 0, bottom: 0, left: 0, right: 0, style: textStyle, string: "OFF" }),
+    ],
+    Behavior: class extends Behavior {
+        onTouchBegan(container) {
+            container.state = 1;
+            application.distribute("onToggleLight", 1);
+        }
+        onTouchEnded(container) {
+            container.state = 0;
+            application.distribute("onToggleLight", 0);
+        }
+        onToggleLight(container, value) {
+            container.statusString.string = (value) ? "ON" : "OFF";
+        }
+    }
+}));
